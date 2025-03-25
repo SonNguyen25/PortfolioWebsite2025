bc97ffc79dba111f54175a0948af1a000c2d7370
sonnguyen25 <ssbaoson@gmail.com>
2025-03-25 05:23:36 -0400

    temporarily stop imagemin

--- vite.config.js
+++ vite.config.js
@@ -17,37 +17,37 @@ export default defineConfig({
     //   ext: ".gz",
     //   deleteOriginFile: false,
     // }),
-    imagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 80,
-      },
-      pngquant: {
-        quality: [0.8, 0.9],
-        speed: 4,
-      },
-      svgo: {
-        plugins: [
-          {
-            name: "removeViewBox",
-            active: false,
-          },
-          {
-            name: "removeEmptyAttrs",
-            active: false,
-          },
-        ],
-      },
-      webp: {
-        quality: 80,
-      },
-    }),
+    // imagemin({
+    //   gifsicle: {
+    //     optimizationLevel: 7,
+    //     interlaced: false,
+    //   },
+    //   optipng: {
+    //     optimizationLevel: 7,
+    //   },
+    //   mozjpeg: {
+    //     quality: 80,
+    //   },
+    //   pngquant: {
+    //     quality: [0.8, 0.9],
+    //     speed: 4,
+    //   },
+    //   svgo: {
+    //     plugins: [
+    //       {
+    //         name: "removeViewBox",
+    //         active: false,
+    //       },
+    //       {
+    //         name: "removeEmptyAttrs",
+    //         active: false,
+    //       },
+    //     ],
+    //   },
+    //   webp: {
+    //     quality: 80,
+    //   },
+    // }),
   ],
 
   assetsInclude: ["**/*.glb"],
